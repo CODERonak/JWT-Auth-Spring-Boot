123a27a770f23cafc3bac0d35b0c5a1c69386275
r9988.2588 <r9988.2588@gmail.com>
2025-05-15 09:02:36 +0000

    first commit

---
+++ src/main/java/com/code/JWTAuth/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package com.code.JWTAuth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true)
+    private String username;
+    private String password;
+}
